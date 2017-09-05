ae452c8ae1bcaaf255897e833ff0580b3d71c2f7
Jack Grahl <jack.grahl@gmail.com>
2017-09-05 22:41:30 +0000

    Add an empty tests package as a placeholder.

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup
 
-setup(name='lsd',
-      version='0.1.0',
-      description='Pounds, shillings and pence conversion',
-      url='http://github.com/jwg4/lsd',
-      author='Jack Grahl',
-      author_email='jack.grahl@gmail.com',
-      license='MIT',
-      packages=['lsd'],
-      zip_safe=False
+setup(
+    name='lsd',
+    version='0.1.0',
+    description='Pounds, shillings and pence conversion',
+    url='http://github.com/jwg4/lsd',
+    author='Jack Grahl',
+    author_email='jack.grahl@gmail.com',
+    license='MIT',
+    packages=['lsd'],
+    zip_safe=False,
+    test_suite="tests"
 )
---
+++ tests/test_round_trip.py
@@ -0,0 +1,5 @@
+import unittest
+
+
+class TestRoundTrips(unittest.TestCase):
+    pass
