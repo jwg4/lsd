92db4c835fe8139508f88d9cd7c2a174a843d463
Jack Grahl <jack.grahl@gmail.com>
2017-09-05 23:02:07 +0000

    Move this stuff out into a function.

--- lsd/__init__.py
+++ lsd/__init__.py
@@ -8,20 +8,24 @@ def pounds_and_new_pence(l, s, d):
     return l + s / 20 + d / 240
 
 
+def _get_granularity_multiplier(granularity):
+    if granularity == "penny":
+        return 1
+    elif granularity == "hapenny":
+        return 2
+    elif granularity == "farthing":
+        return 4
+    else:
+        raise Exception("Not a correct granularity specification: %s." % granularity)
+
+
 def pounds_shillings_and_pence(x, rounding="nearest", granularity="penny"):
     l = int(x)
     x = x - l
     s = int(x * 20)
     x = x * 20 - s
 
-    if granularity == "penny":
-        k = 1
-    elif granularity == "hapenny":
-        k = 2
-    elif granularity == "farthing":
-        k = 4
-    else:
-        raise Exception("Not a correct granularity specification: %s." % granularity)
+    k = _get_granularity_multiplier(granularity)
 
     if rounding == "nearest":
         d = round(x * 12 * k) / k
