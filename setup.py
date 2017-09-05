6b68ee8184f153530cbb650e24dedde6c1d23b76
Jack Grahl <jack.grahl@gmail.com>
2017-09-05 23:11:48 +0000

    Bump version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='lsd',
-    version='0.1.0',
+    version='0.1.1',
     description='Pounds, shillings and pence conversion',
     url='http://github.com/jwg4/lsd',
     author='Jack Grahl',
