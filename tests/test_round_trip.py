f6f3fa1be6085f979e663ca374f64eaa915a0537
Jack Grahl <jack.grahl@gmail.com>
2017-09-05 23:53:39 +0000

    Add a basic test using hypothesis.

--- tests/test_round_trip.py
+++ tests/test_round_trip.py
@@ -1,5 +1,20 @@
 import unittest
 
+from hypothesis import given
+from hypothesis.strategies import integers, decimals
+
+from lsd import pounds_and_new_pence, pounds_shillings_and_pence
+
 
 class TestRoundTrips(unittest.TestCase):
-    pass
+    @given(
+        l=integers(min_value=0, max_value=100),
+        s=integers(min_value=0, max_value=19),
+        d=decimals(min_value=0, max_value=11.99)
+    )
+    def test_round_trip(self, l, s, d):
+        new_money = pounds_and_new_pence(l, s, d)
+        l_, s_, d_ = pounds_shillings_and_pence(new_money, rounding="fraction")
+        self.assertEqual(l_, l)
+        self.assertEqual(s_, s)
+        self.assertEqual(d_, d)
