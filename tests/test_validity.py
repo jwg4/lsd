5dbc2f16fa117edfddddde9c2a6a5f047827fba0
Jack Grahl <jack.grahl@gmail.com>
2017-09-12 22:23:33 +0000

    Add tests for the conversion which might fail.
    We could get a ConversionError but it shouldn't break any other way.

--- tests/test_validity.py
+++ tests/test_validity.py
@@ -4,6 +4,7 @@ from hypothesis import given
 from hypothesis.strategies import integers, decimals
 
 from lsd import pounds_and_new_pence, pounds_shillings_and_pence
+from lsd.error import ConversionError
 
 
 class TestSuccessfulConversion(unittest.TestCase):
@@ -23,3 +24,28 @@ class TestSuccessfulConversion(unittest.TestCase):
         old_money = pounds_shillings_and_pence(p, rounding="nearest")
         self.assertIsNotNone(old_money)
         self.assertEqual(len(old_money), 3)
+
+
+class TestPossiblySuccessfulConversion(unittest.TestCase):
+    @given(
+        l=integers(min_value=0),
+        s=integers(min_value=0, max_value=19),
+        d=decimals(min_value=0, max_value=11.99)
+    )
+    def test_convert_to_new_money(self, l, s, d):
+        try:
+            new_money = pounds_and_new_pence(l, s, d, rounding="strict")
+            self.assertIsNotNone(new_money)
+        except ConversionError:
+            pass
+
+    @given(
+        p=decimals(min_value=0, allow_nan=False, allow_infinity=False)
+    )
+    def test_convert_to_old_money(self, p):
+        try:
+            old_money = pounds_shillings_and_pence(p, rounding="strict")
+            self.assertIsNotNone(old_money)
+            self.assertEqual(len(old_money), 3)
+        except ConversionError:
+            pass
