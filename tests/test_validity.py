80d6331fe44570aa7cbc5de9819cb7e90f472eb7
Jack Grahl <jack.grahl@gmail.com>
2017-09-12 21:56:57 +0000

    Add tests which just check that conversion gives us something.

---
+++ tests/test_validity.py
@@ -0,0 +1,25 @@
+import unittest
+
+from hypothesis import given
+from hypothesis.strategies import integers, decimals
+
+from lsd import pounds_and_new_pence, pounds_shillings_and_pence
+
+
+class TestSuccessfulConversion(unittest.TestCase):
+    @given(
+        l=integers(min_value=0),
+        s=integers(min_value=0, max_value=19),
+        d=decimals(min_value=0, max_value=11.99)
+    )
+    def test_convert_to_new_money(self, l, s, d):
+        new_money = pounds_and_new_pence(l, s, d, rounding="nearest")
+        self.assertIsNotNone(new_money)
+
+    @given(
+        p=decimals(min_value=0)
+    )
+    def test_convert_to_old_money(self, p):
+        old_money = pounds_shillings_and_pence(p, rounding="nearest")
+        self.assertIsNotNone(old_money)
+        self.assertEqual(len(old_money), 3)
